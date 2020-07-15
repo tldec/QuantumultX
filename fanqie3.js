7f7384234803b0fefabb6a7a9e139a6d50f4b159
Long Tan <45587307+tldec@users.noreply.github.com>
2020-07-16 00:10:22 +0800

    Create fanqie3.js

---
+++ fanqie3.js
@@ -0,0 +1,14 @@
+/*
+{
+    "code":0,
+    "data":{
+        "interactivation_count":"0",
+        "notification_count":"0",
+        "total":"0"
+    },
+    "message":"success"
+}
+*/
+var obj = JSON.parse($response.body);
+obj.ad_item = null;
+$done({body:JSON.stringify(obj)});
