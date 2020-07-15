596064c57ca8aeccaeb03eccd1e14f7c792f44ae
Long Tan <45587307+tldec@users.noreply.github.com>
2020-07-15 23:07:54 +0800

    更新视频广告过滤规则

--- fanqie.js
+++ fanqie.js
@@ -1,3 +1,21 @@
+
+/*
+{
+    "video_info":{
+        "data":{
+            "dynamic_video":null,
+            "dns_info":null
+        },
+        "message":"success",
+        "code":0,
+        "total":4
+    },
+    "message":"success",
+    "code":0,
+    "data":{
+    }
+}
+*/
 var obj = JSON.parse($response.body);
-obj.data = {};
+obj.video_info = {};
 $done({body:JSON.stringify(obj)});
